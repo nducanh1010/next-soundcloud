1158f3c1474bfabe7deae5dfa1563ae96e23001b
DESKTOP-JU1F7QJ\admin <nducanh1010@gmail.com>
2023-10-17 11:24:17 +0700

    update footer

--- src/components/footer/app.footer.tsx
+++ src/components/footer/app.footer.tsx
@@ -8,7 +8,6 @@ const AppFooter = () => {
   if (!hasMounted) return <></>;
   return (
     <div>
-<<<<<<< HEAD
       <AppBar
         position="fixed"
         sx={{ top: "auto", bottom: 0, background: "#f2f2f2" }}
@@ -38,15 +37,6 @@ const AppFooter = () => {
             <div style={{ color: "#ccc" }}>DA</div>
             <div style={{ color: "black" }}>Who i am ?</div>
           </div>
-=======
-      <AppBar position="fixed" color="primary" sx={{ top: "auto", bottom: 0 }}>
-        <Container>
-          <AudioPlayer
-            src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3"
-            volume={0.5}
-            // Try other props!
-          />
->>>>>>> aebdb0a34aad65083e98fdc8628382ea0cd829be
         </Container>
       </AppBar>
     </div>
