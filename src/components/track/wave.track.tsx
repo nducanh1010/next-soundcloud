885489742d5b0efb2f0677d5bae962ad3206cfb7
DESKTOP-JU1F7QJ\admin <nducanh1010@gmail.com>
2023-10-20 23:40:51 +0700

    add fake data, coment

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -1,29 +1,51 @@
 "use client";
-import { useWavesurfer } from "@/utils/customHook";
+
+import { useEffect, useRef, useState, useMemo, useCallback } from "react";
 import { useSearchParams } from "next/navigation";
-import React, {
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from "react";
+import { useWavesurfer } from "@/utils/customHook";
 import { WaveSurferOptions } from "wavesurfer.js";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import PauseIcon from "@mui/icons-material/Pause";
 import "./wave.scss";
+
 const WaveTrack = () => {
   const searchParams = useSearchParams();
-  const fileName = searchParams.get("audio"); // laays data param
+  const fileName = searchParams.get("audio");
   const containerRef = useRef<HTMLDivElement>(null);
   const hoverRef = useRef<HTMLDivElement>(null);
+  const arrComments = [
+    {
+      id: 1,
+      avatar: "http://localhost:8000/images/chill1.png",
+      moment: 10,
+      user: "username 1",
+      content: "just a comment1",
+    },
+    {
+      id: 2,
+      avatar: "http://localhost:8000/images/chill1.png",
+      moment: 30,
+      user: "username 2",
+      content: "just a comment3",
+    },
+    {
+      id: 3,
+      avatar: "http://localhost:8000/images/chill1.png",
+      moment: 50,
+      user: "username 3",
+      content: "just a comment3",
+    },
+  ];
+
   const [time, setTime] = useState<string>("0:00");
   const [duration, setDuration] = useState<string>("0:00");
-  const optionMemo = useMemo((): Omit<WaveSurferOptions, "container"> => {
-    // Define the waveform gradient
-    let gradient;
-    let progressGradient;
+
+  const optionsMemo = useMemo((): Omit<WaveSurferOptions, "container"> => {
+    let gradient, progressGradient;
     if (typeof window !== "undefined") {
-      const canvas = document.createElement("canvas")!;
+      const canvas = document.createElement("canvas");
       const ctx = canvas.getContext("2d")!;
+      // Define the waveform gradient
       gradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35);
       gradient.addColorStop(0, "#656666"); // Top color
       gradient.addColorStop((canvas.height * 0.7) / canvas.height, "#656666"); // Top color
@@ -67,24 +89,31 @@ const WaveTrack = () => {
       ); // Bottom color
       progressGradient.addColorStop(1, "#F6B094"); // Bottom color
     }
+
     return {
-      // waveColor: "rgb(200,0,200)",
-      height: 150,
-      barWidth: 2,
       waveColor: gradient,
       progressColor: progressGradient,
+      height: 100,
+      barWidth: 3,
       url: `/api?audio=${fileName}`,
     };
   }, []);
-  const wavesurfer = useWavesurfer(containerRef, optionMemo);
+  const wavesurfer = useWavesurfer(containerRef, optionsMemo);
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+  // Initialize wavesurfer when the container mounts
+  // or any of the props change
   useEffect(() => {
     if (!wavesurfer) return;
     setIsPlaying(false);
-    const timeEl = time!;
-    const durationEl = duration!;
+
     const hover = hoverRef.current!;
     const waveform = containerRef.current!;
+    waveform.addEventListener(
+      "pointermove",
+      (e) => (hover.style.width = `${e.offsetX}px`)
+    );
+
     const subscriptions = [
       wavesurfer.on("play", () => setIsPlaying(true)),
       wavesurfer.on("pause", () => setIsPlaying(false)),
@@ -94,23 +123,22 @@ const WaveTrack = () => {
       wavesurfer.on("timeupdate", (currentTime) => {
         setTime(formatTime(currentTime));
       }),
-      waveform.addEventListener(
-        "pointermove",
-        (e) => (hover.style.width = `${e.offsetX}px`)
-      ),
+      wavesurfer.once("interaction", () => {
+        wavesurfer.play();
+      }),
     ];
 
     return () => {
       subscriptions.forEach((unsub) => unsub());
     };
   }, [wavesurfer]);
+
   // On play button click
   const onPlayClick = useCallback(() => {
     if (wavesurfer) {
       wavesurfer.isPlaying() ? wavesurfer.pause() : wavesurfer.play();
     }
   }, [wavesurfer]);
-  // Current time & duration
 
   const formatTime = (seconds: number) => {
     const minutes = Math.floor(seconds / 60);
@@ -118,20 +146,137 @@ const WaveTrack = () => {
     const paddedSeconds = `0${secondsRemainder}`.slice(-2);
     return `${minutes}:${paddedSeconds}`;
   };
+  const calculateLeft = (moment: number) => {
+    const hardCodeDuration = 199;
+    const percent = (moment / hardCodeDuration) * 100;
+    return `${percent}%`;
+  };
 
   return (
-    <>
-      <div className="wave-form-container">
-        <div ref={containerRef}></div>
-        <div className="time">{time}</div>
-        <div className="duration">{duration}</div>
-        <div ref={hoverRef} className="hover-wave"></div>
+    <div style={{ marginTop: 20 }}>
+      <div
+        style={{
+          display: "flex",
+          gap: 15,
+          padding: 20,
+          height: 400,
+          background:
+            "linear-gradient(135deg, rgb(106, 112, 67) 0%, rgb(11, 15, 20) 100%)",
+        }}
+      >
+        <div
+          className="left"
+          style={{
+            width: "75%",
+            height: "calc(100% - 10px)",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <div className="info" style={{ display: "flex" }}>
+            <div>
+              <div
+                onClick={() => onPlayClick()}
+                style={{
+                  borderRadius: "50%",
+                  background: "#f50",
+                  height: "50px",
+                  width: "50px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  cursor: "pointer",
+                }}
+              >
+                {isPlaying === true ? (
+                  <PauseIcon sx={{ fontSize: 30, color: "white" }} />
+                ) : (
+                  <PlayArrowIcon sx={{ fontSize: 30, color: "white" }} />
+                )}
+              </div>
+            </div>
+            <div style={{ marginLeft: 20 }}>
+              <div
+                style={{
+                  padding: "0 5px",
+                  background: "#333",
+                  fontSize: 30,
+                  width: "fit-content",
+                  color: "white",
+                }}
+              >
+                DucAnh's Songs
+              </div>
+              <div
+                style={{
+                  padding: "0 5px",
+                  marginTop: 10,
+                  background: "#333",
+                  fontSize: 20,
+                  width: "fit-content",
+                  color: "white",
+                }}
+              >
+                DA
+              </div>
+            </div>
+          </div>
+          <div ref={containerRef} className="wave-form-container">
+            <div className="time">{time}</div>
+            <div className="duration">{duration}</div>
+            <div ref={hoverRef} className="hover-wave"></div>
+            <div
+              className="overlay"
+              style={{
+                position: "absolute",
+                height: "30px",
+                width: "100%",
+                bottom: "0",
+                // background: "#ccc"
+                backdropFilter: "brightness(0.5)",
+              }}
+            ></div>
+            <div className="comments" style={{ position: "relative" }}>
+              {arrComments.map((item) => {
+                return (
+                  <img
+                    key={item.id}
+                    style={{
+                      width: "20px",
+                      height: "20px",
+                      position: "absolute",
+                      top: 71,
+                      zIndex: 20,
+                      left: calculateLeft(item.moment),
+                    }}
+                    src="http://localhost:8000/images/chill1.png"
+                  />
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <div
+          className="right"
+          style={{
+            width: "25%",
+            padding: 15,
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              background: "#ccc",
+              width: 250,
+              height: 250,
+            }}
+          ></div>
+        </div>
       </div>
-
-      <button onClick={() => onPlayClick()}>
-        {isPlaying === true ? "Pause" : "Play"}
-      </button>
-    </>
+    </div>
   );
 };
+
 export default WaveTrack;
