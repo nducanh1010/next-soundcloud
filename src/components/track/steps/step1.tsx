b26385cd1aa1ec3db48c8e711a8c8aa81a4993b9
DESKTOP-JU1F7QJ\admin <nducanh1010@gmail.com>
2023-10-27 15:41:55 +0700

    add ãxios uplaod file

--- src/components/track/steps/step1.tsx
+++ src/components/track/steps/step1.tsx
@@ -1,9 +1,13 @@
+"use client";
 import { useDropzone, FileWithPath } from "react-dropzone";
 import "./theme.css";
 import { styled } from "@mui/material/styles";
 import Button from "@mui/material/Button";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import { useCallback } from "react";
+import { sendFileRequest, sendRequest } from "@/utils/api";
+import { useSession } from "next-auth/react";
+import axios from "axios";
 const InputFileUpload = () => {
   return (
     <Button
@@ -30,13 +34,53 @@ const VisuallyHiddenInput = styled("input")({
 });
 
 const Step1 = () => {
-  const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
-    // Do something with the files
-  }, []);
+  const { data: session } = useSession(); // gán session vào data
+  /*
+  Thực hiện api upload tracks
+  */
+  const onDrop = useCallback(
+    async (acceptedFiles: FileWithPath[]) => {
+      // Do something with the files
+      if (acceptedFiles && acceptedFiles[0]) {
+        const audio = acceptedFiles[0];
+        const formData = new FormData();
+        console.log("session ", session?.access_token);
+        formData.append("fileUpload", audio);
+        // const chills = await sendFileRequest<IBackendRes<ITrackTop[]>>({
+        //   headers: {
+        //     Authorization: `Bearer ${session?.access_token}`,
+        //     target_type: "tracks",
+        //   },
+        //   url: "http://localhost:8000/api/v1/files/upload",
+        //   method: "POST",
+        //   body: formData,
+        // });
+        try {
+          const res = await axios.post(
+            "http://localhost:8000/api/v1/files/upload",
+            formData,
+            {
+              headers: {
+                Authorization: `Bearer ${session?.access_token}`,
+                target_type: "tracks",
+              },
+            }
+          );
+        } catch (error) {
+          //@ts-ignore
+          console.log(error?.response?.data.message);
+        }
+      }
+    },
+    [session]
+  ); // hcayj khi biến session thay đổi, lúc đầu session bằng udefined
+  // truyền option của file trong use Drop zone
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
     onDrop,
+    accept: {
+      audio: [".mp3", ".m4a", ".wav"],
+    },
   });
-
   const files = acceptedFiles.map((file: FileWithPath) => (
     <li key={file.path}>
       {file.path} - {file.size} bytes
